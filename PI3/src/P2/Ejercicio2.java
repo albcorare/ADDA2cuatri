126e703d1e5da29d200dff20ef9147aef9aae9cb
albcorare <thealbertuswtf@gmail.com>
2019-03-20 12:44:14 +0100

    grafos0.4

--- PI3/src/P2/Ejercicio2.java
+++ PI3/src/P2/Ejercicio2.java
@@ -3,6 +3,9 @@ package P2;
 import java.io.PrintWriter;
 
 import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.SimpleWeightedGraph;
 import org.jgrapht.io.DOTExporter;
 import org.jgrapht.io.IntegerComponentNameProvider;
@@ -10,13 +13,53 @@ import us.lsi.common.Files2;
 import us.lsi.graphs.GraphsReader;
 
 public class Ejercicio2 {
-	
-	public static void main(String[] args) {
-		Graph<Ciudad, Carretera> g = cargarGrafo("./ficheros/Andalucia.txt");
-		exportarGrafo(g, "./ficheros/Andalucia.gv");
 
-		System.out.println(g);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Graph<Ciudad, Carretera> g = cargarGrafo("./ficheros/Andalucia.txt");
+		Graph<Ciudad, Carretera> g2 = cargarGrafo("./ficheros/Andalucia.txt");
+		g.edgeSet().forEach(a -> g.setEdgeWeight(a, a.getCoste())); // repesar aristas
+		g2.edgeSet().forEach(a -> g2.setEdgeWeight(a, a.getTiempo())); // repesar aristas
+		apartadoA(g, "Huelva", "Almeria");
+		apartadoA2(g2, "Huelva", "Almeria");
 	}
+
+	private static void apartadoA(Graph<Ciudad, Carretera> g, String o, String d) { // jgraph
+		System.out.println("----------------APARTADO A----------------");
+		ShortestPathAlgorithm<Ciudad, Carretera> alg = new DijkstraShortestPath<>(g);
+		Ciudad from = Ciudad.create(o);
+		Ciudad to = Ciudad.create(d);
+		GraphPath<Ciudad, Carretera> gp = alg.getPath(from, to);
+		System.out.println("Lista de ciudades: " + gp.getVertexList());
+		System.out.println("Coste total: " + gp.getWeight());
+		System.out.println("Numero de carreteras: " + gp.getLength());
+	}
+
+	private static void apartadoA2(Graph<Ciudad, Carretera> g, String o, String d) { // jgraph
+		System.out.println("----------------APARTADO A2----------------");
+		ShortestPathAlgorithm<Ciudad, Carretera> alg = new DijkstraShortestPath<>(g);
+		Ciudad from = Ciudad.create(o);
+		Ciudad to = Ciudad.create(d);
+		GraphPath<Ciudad, Carretera> gp = alg.getPath(from, to);
+		System.out.println("Lista de ciudades: " + gp.getVertexList());
+		System.out.println("Coste total: " + gp.getWeight());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	private static void exportarGrafo(Graph<Ciudad, Carretera> g, String nf) { // nf = nombre fichero
 		DOTExporter<Ciudad, Carretera> de = new DOTExporter<Ciudad, Carretera>(new IntegerComponentNameProvider<>(),

8ed31e224a06f94f38f15a02ef9f376480e18bdc
albcorare <thalbertuswtf@gmail.com>
2019-03-22 10:48:06 +0100

    Grafos0.7

--- PI3/src/P2/Ejercicio2.java
+++ PI3/src/P2/Ejercicio2.java
@@ -1,10 +1,16 @@
 package P2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.alg.spanning.KruskalMinimumSpanningTree;
 import org.jgrapht.alg.tour.HeldKarpTSP;
 import org.jgrapht.graph.SimpleWeightedGraph;
 import us.lsi.graphs.GraphsReader;
@@ -15,13 +21,28 @@ public class Ejercicio2 {
 		// TODO Auto-generated method stub
 		Graph<Ciudad, Carretera> g = cargarGrafo("./ficheros/Ejemplo.txt");
 		Graph<Ciudad, Carretera> g2 = cargarGrafo("./ficheros/Ejemplo.txt");
+		Graph<Ciudad, Carretera> g3 = cargarGrafo("./ficheros/Ejemplo.txt");
 		g.edgeSet().forEach(a -> g.setEdgeWeight(a, a.getCoste())); // repesar aristas
 		g2.edgeSet().forEach(a -> g2.setEdgeWeight(a, a.getTiempo())); // repesar aristas
+		
+		Ciudad c1 = new Ciudad("Lugar0");
+		Ciudad c2 = new Ciudad("Lugar1");
+		Ciudad c3 = new Ciudad("Lugar2");
+		Ciudad c4 = new Ciudad("Lugar9");
+		Set<Ciudad> ciudades = new HashSet<>();
+		ciudades.add(c1);
+		ciudades.add(c2);
+		ciudades.add(c3);
+		ciudades.add(c4);
+	
+		
+		
 		apartadoA(g, "Lugar0", "Lugar9");
 		apartadoA1(g2,"Lugar0", "Lugar9");
 		apartadoA2(g2, "Lugar7", "Lugar0");
 		apartadoA21(g2, "Lugar7", "Lugar0");
-		apartadoB(g2, "Lugar0");
+		apartadoB(g3, "Lugar0");
+		apartadoC(g, ciudades);
 	}
 
 	private static void apartadoA(Graph<Ciudad, Carretera> g, String o, String d) { // jgraph
@@ -65,12 +86,41 @@ public class Ejercicio2 {
 		System.out.println( ciclo.getTour(g).getVertexList());
 	}
 	
-	private static void apartadoC(Graph<Ciudad, Carretera> g, String o, List<Ciudad> lista) { // jgraph
-		System.out.println("----------------APARTADO B----------------");
-		HeldKarpTSP<Ciudad, Carretera> ciclo = new HeldKarpTSP<>();
-		System.out.println(ciclo.getTour(g).getVertexList());
+	private static void apartadoC(Graph<Ciudad, Carretera> g, Set<Ciudad> ciudades) {
+		System.out.println("----------------APARTADO C----------------");
+		List<Ciudad> borrar = new ArrayList<>();
+		Set<Carretera> res = new HashSet<>();
+		Graph<Ciudad, Carretera> grafo = new SimpleWeightedGraph<>(Carretera.class);
+		for(Ciudad c: g.vertexSet()) {
+			if(!ciudades.contains(c)) {
+				ciudades.remove(c);
+			}else {
+				grafo.addVertex(c);
+			}
+		}	
+
+		System.out.println(grafo);
 	}
-	
+		
+		
+		
+		
+//		for (Ciudad c : g.vertexSet()) {
+//			if (!ciudades.contains(c)) {
+//				borrar.add(c);
+//			}
+//		}
+//		g.removeAllVertices(borrar);
+//		ConnectivityInspector<Ciudad, Carretera> co = new ConnectivityInspector<>(g);
+//		if (co.isConnected()) {
+//			KruskalMinimumSpanningTree<Ciudad, Carretera> k = new KruskalMinimumSpanningTree<>(g);
+//			res=  k.getSpanningTree().getEdges();
+//		} else {
+//			res= new HashSet<>();
+//		}
+//		System.out.println(res);
+
+
 	
 	
 	
